77af1eee1d5f4b54439b52184fc80b71fe767ca5
jules <julianpeters@web.de>
2016-02-11 19:19:56 +0100

    Neue Testdaten, closes #21

--- src/main/java/de/energienetz/hupe/HupeChartBuilder.java
+++ src/main/java/de/energienetz/hupe/HupeChartBuilder.java
@@ -1,7 +1,6 @@
 package de.energienetz.hupe;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
