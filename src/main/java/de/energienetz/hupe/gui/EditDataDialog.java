488c836377e643865592d0b79d212fb0de6a8ae2
jules <julianpeters@web.de>
2016-02-11 19:45:34 +0100

    Default-Namen für die Kurven geändert, closes #17

--- src/main/java/de/energienetz/hupe/gui/EditDataDialog.java
+++ src/main/java/de/energienetz/hupe/gui/EditDataDialog.java
@@ -25,8 +25,7 @@ import de.energienetz.hupe.HupeColor;
 public class EditDataDialog extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 9217860900420979997L;
 	private static final String okAction = "okAction";
-	private static final String[] defaultNames = { "Vorlauf", "Rücklauf", "Raumtemperatur", "Warmwasser", "Wohnraum", "Heizkörper", //
-			"Kollektor Vorlauf", "Kollektor Rücklauf", "Speicher oben", "Speicher mitte", "Speicher unten", "<anderes>" };
+	private static final String[] defaultNames = { "VLHzg", "VLKessel", "Ruecklauf", "Warmw", "Aussen", "Wohnen", "Speicher", "<anderes>" };
 	private final HupeGui mainGui;
 
 	public EditDataDialog(final HupeGui mainGui) {
